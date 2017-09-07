c7b58d0139ab06cc33ce44c69d8a379b605e57d2
TerrenceLeung <leungzeeyan@gmail.com>
2017-09-07 23:53:54 +0800

    多线程测试

---
+++ src/main/java/utils/ThreadTest.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by liangziyan on 2017/9/6.
+ */
+public class ThreadTest {
+
+    public static void main(String[] args){
+
+        Semaphore semaphore = new Semaphore(2);
+        for(int i=0;i<10;i++){
+            new MyThread(semaphore).start();
+        }
+    }
+
+}
+
+class MyThread extends Thread{
+
+    private Semaphore semaphore = null;
+
+    public MyThread(Semaphore semaphore){
+       this.semaphore = semaphore;
+    }
+
+    public void run(){
+        try{
+            semaphore.acquire();
+            System.out.println(getName());
+            sleep(2000);
+
+        }catch (Exception w){
+
+        }
+       semaphore.release();
+
+    }
+}
+
