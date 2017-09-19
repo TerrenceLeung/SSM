5089bb8b711098d477c626bbcb6273ca09312924
TerrenceLeung <leungzeeyan@gmail.com>
2017-09-19 23:41:32 +0800

    add 多线程测试

---
+++ src/main/java/test/QueueTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by liangziyan on 2017/9/18.
+ */
+public class QueueTest {
+
+    public QueueTest(){
+
+    }
+
+    public void test(){
+        ConcurrentHashMap
+    }
+}
